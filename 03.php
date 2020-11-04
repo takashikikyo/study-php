e1865eb9663ceef9de4b3af5c996cc92c94655bc
”takashikikyo” <takashikikyo@gmail.com>
2020-11-04 06:15:06 +0000

    PHP/Laravel 03 課題提出

---
+++ 03.php
@@ -0,0 +1,36 @@
+<?php
+$name = "崇";
+if ($name == "崇") {
+    echo "私は".$name."です";
+} else {
+    echo $name."ではありません";
+}
+?>
+
+<?php 
+$total = 0;
+
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+?>
+
+
+<?php 
+$fruit = ["梨 ", "ぶどう ", "パイナップル ", "いちご ", "さくらんぼ "];
+foreach ($fruit as $fruit) {
+    echo $fruit;
+}
+?>
+
+<?php
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+  if($i % 5 == 0){
+    echo $i."\n";
+  }
+}
+?>
